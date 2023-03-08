44f009fd65597c2b2e4269c9820efa4059fb06dd
Benjamin Hall <benjamin1995hall@gmail.com>
2023-03-08 21:43:41 +0000

    fuck this

---
+++ app/models/review.rb
@@ -0,0 +1,13 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: true
+  validates :rating, presence: true, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 0,
+    less_than_or_equal_to: 5
+  }
+end
+
+# validates :rating, numericality: { only_integer: true }
+# validates :rating, inclusion: { in: 0..5 }
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts 'cleaning the database...'
+Restaurant.destroy_all
+
+puts 'creating restaurants...'
+
+Restaurant.create!(
+  [
+    {
+      name: 'Pizzeria Uno',
+      address: 'London',
+      phone_number: '+44 20 1234 5678',
+      category: 'italian'
+    },
+    {
+      name: 'La Taqueria',
+      address: 'Manchester',
+      phone_number: '+44 20 8765 4321',
+      category: 'french'
+    },
+    {
+      name: 'Sushi Yasuda',
+      address: 'Birmingham',
+      phone_number: '+44 20 3456 7890',
+      category: 'japanese'
+    },
+    {
+      name: 'Thai Basil',
+      address: 'Glasgow',
+      phone_number: '+44 20 9876 5432',
+      category: 'chinese'
+    },
+    {
+      name: 'Hunan Palace',
+      address: 'Belfast',
+      phone_number: '+44 20 2345 6789',
+      category: 'chinese'
+    }
+  ]
+)
